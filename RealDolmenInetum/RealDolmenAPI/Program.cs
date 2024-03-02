8877f05a29d959aa8b8831ddbfb7350f92e85235
Adil-DENDE <adil.lakhloufi@student.odisee.be>
2024-03-02 11:33:15 +0100

    API PROJECT SETUP

---
+++ RealDolmenInetum/RealDolmenAPI/Program.cs
@@ -0,0 +1,24 @@
+// CREATIE VAN DE WEB APP ENVIRONMENT
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// SWAGGER ====== TOONT EEN SOORT PAGE MET DOCU VAN API
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+
+// ENDPOINT OM TE TESTEN
+app.MapGet("/User", () => "THIS IS A TEST");
+
+
+
+
+app.Run();
+
+
---
+++ RealDolmenInetum/RealDolmenAPI/User.cs
@@ -0,0 +1,15 @@
+﻿namespace RealDolmenAPI
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int ManagerId { get; set; }
+        public int NiveauId { get; set; }
+        public string FirstName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthdate { get; set; }
+        public DateTime StartDate {  get; set; }       
+    }
+}
