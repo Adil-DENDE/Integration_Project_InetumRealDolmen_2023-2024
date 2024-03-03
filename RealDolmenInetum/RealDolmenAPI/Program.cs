17d675fd99688b14b34e00822f667fdfc3b019b1
Adil-DENDE <adil.lakhloufi@student.odisee.be>
2024-03-03 19:17:51 +0100

    GET ENDPOINT OM 1 BEPAALD USER TE ZIEN

--- RealDolmenInetum/RealDolmenAPI/Program.cs
+++ RealDolmenInetum/RealDolmenAPI/Program.cs
@@ -1,6 +1,7 @@
 // CREATIE VAN DE WEB APP ENVIRONMENT
 using Microsoft.EntityFrameworkCore;
 using RealDolmenAPI.Data;
+using RealDolmenAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
@@ -21,8 +22,13 @@ app.UseHttpsRedirection();
 
 
 // ENDPOINT OM ALLE BENCHERS TE ZIEN //WE ZIEN NU NOG ALTIJD ALLE USERS (MANAGERS STAAN ER TUSSEN).
-app.MapGet("/Benchers", async (AppDbContext db) =>  await db.Users.ToListAsync());
-    
+app.MapGet("/Benchers", async (AppDbContext db) =>  await db.User.ToListAsync());
+
+
+// ENDPOINT ALS WE DE DATA VAN EEN BENCHER ZOUDEN WILLEN ZIEN 
+app.MapGet("/Benchers/{id:int}", async (int id, AppDbContext db) => await db.User.FindAsync(id) is User user ? Results.Ok(user) : Results.NotFound());
+
+
 
 
 
