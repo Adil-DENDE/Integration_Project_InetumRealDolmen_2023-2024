4cdb12b95e50bdbe94f0f94751cf48b067c960cc
Adil-DENDE <adil.lakhloufi@student.odisee.be>
2024-03-04 15:19:10 +0100

    Controller Folder aangemaakt
    UserController toegevoegd

---
+++ RealDolmenInetum/RealDolmenAPI/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModelLibrary.Data;
+using ModelLibrary.Models;
+
+namespace RealDolmenAPI.Controllers
+{
+    public class UserController
+    {
+        public static void Map(WebApplication app)
+        {
+            // ENDPOINT OM ALLE BENCHERS TE ZIEN //WE ZIEN NU NOG ALTIJD ALLE USERS (MANAGERS STAAN ER TUSSEN).
+            app.MapGet("/Benchers", async (AppDbContext db) => await db.User.ToListAsync());
+
+            // ENDPOINT ALS WE DE DATA VAN EEN BENCHER ZOUDEN WILLEN ZIEN 
+            app.MapGet("/Benchers/{id:int}", async (int id, AppDbContext db) => await db.User.FindAsync(id) is User user ? Results.Ok(user) : Results.NotFound());
+        }
+    }
+}
--- RealDolmenInetum/RealDolmenAPI/Program.cs
+++ RealDolmenInetum/RealDolmenAPI/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using ModelLibrary.Data;
 using ModelLibrary.Models;
+using RealDolmenAPI.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
@@ -11,6 +12,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>();
 
 var app = builder.Build();
+UserController.Map(app);
 
 // SWAGGER ====== TOONT EEN SOORT PAGE MET DOCU VAN API
 if (app.Environment.IsDevelopment())
@@ -19,18 +21,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 app.UseHttpsRedirection();
-
-
-// ENDPOINT OM ALLE BENCHERS TE ZIEN //WE ZIEN NU NOG ALTIJD ALLE USERS (MANAGERS STAAN ER TUSSEN).
-app.MapGet("/Benchers", async (AppDbContext db) =>  await db.User.ToListAsync());
-
-
-// ENDPOINT ALS WE DE DATA VAN EEN BENCHER ZOUDEN WILLEN ZIEN 
-app.MapGet("/Benchers/{id:int}", async (int id, AppDbContext db) => await db.User.FindAsync(id) is User user ? Results.Ok(user) : Results.NotFound());
-
-
-
-
 
 app.Run();
 
