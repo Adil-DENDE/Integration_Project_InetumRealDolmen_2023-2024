bb50ac2ffe8d1b96cac3e75f2bf38dd9664a3a35
Adil-DENDE <adil.lakhloufi@student.odisee.be>
2024-03-12 12:16:46 +0100

    Aanpassingen en verbeteringen
    API meer in orde gebracht met de hulp van Cedric en Sana

--- RealDolmenInetum/ModelLibrary/Models/Bench.cs
+++ RealDolmenInetum/ModelLibrary/Models/Bench.cs
@@ -1,23 +1,26 @@
-﻿namespace ModelLibrary.Models
+﻿using ModelLibrary.Dto;
+
+namespace ModelLibrary.Models
 {
     public class Bench
     {
-        public int? Id { get; set; }
-        public int user_id { get; set; }
+        
+        public Bench()
+        {
+            
+        }
+
+        public Bench(int userId, DateTime startBench)
+        {
+            User_id = userId;
+            Start_bench = startBench;
+        }
+        public int Id { get; set; }
+        public int User_id { get; set; }
         public DateTime Start_bench { get; set; }
         public DateTime? End_bench { get; set; }
         public int? Occupation_id { get; set; }
 
-        static public Bench UserbenchToBench(UserBench userBench)
-        {
-            return new Bench {
-                Id = null,
-                user_id = userBench.UserId,
-                
-                Start_bench = userBench.StartBench.HasValue ? userBench.StartBench.Value : DateTime.Now, 
-                End_bench = userBench.EndBench,
-                Occupation_id = null // Aangezien de gebruiker net op de bank wordt geplaatst
-            };
-        }
+      
     }
 }
---
+++ RealDolmenInetum/ModelLibrary/ViewModels/UserViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLibrary.ViewModels
+{
+    public class UserViewModel
+    {
+        public int? Niveau_Id { get; set; }// naam
+        public int? Manager_Id { get; set; }// naam
+        public string Email { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public DateTime Birthdate { get; set; }
+        public DateTime Start_Date { get; set; }// nadenken
+    }
+}
