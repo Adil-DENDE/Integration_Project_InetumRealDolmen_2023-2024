5ef86bc517e575b728005fee42a22d115849750f
Adil-DENDE <adil.lakhloufi@student.odisee.be>
2024-03-13 00:59:10 +0100

    Dashboard View afgemaakt
    Benchers worden correct getoond met al hun datas

--- RealDolmenInetum/ModelLibrary/ViewModels/UserViewModel.cs
+++ RealDolmenInetum/ModelLibrary/ViewModels/UserViewModel.cs
@@ -8,12 +8,13 @@ namespace ModelLibrary.ViewModels
 {
     public class UserViewModel
     {
-        public int? Niveau_Id { get; set; }// naam
-        public int? Manager_Id { get; set; }// naam
-        public string Email { get; set; }
-        public string First_Name { get; set; }
-        public string Last_Name { get; set; }
-        public DateTime Birthdate { get; set; }
-        public DateTime Start_Date { get; set; }// nadenken
+        public int? niveauId { get; set; }// naam
+        //public int? Manager_Id { get; set; }// naam
+        public string mail { get; set; }
+        public string username { get; set; }
+        //public string Last_Name { get; set; }
+        //public DateTime Birthdate { get; set; }
+        public DateTime startBench { get; set; }// nadenken
+        public DateTime? endBench { get; set; }
     }
 }
