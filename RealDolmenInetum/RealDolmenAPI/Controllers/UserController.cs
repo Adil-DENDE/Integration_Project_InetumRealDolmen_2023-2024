08a194ee7ed76c3d775ca939cb22a3d34266d884
marwan-ben-hamman <113838733+marwan-ben-hamman@users.noreply.github.com>
2024-03-17 03:13:54 +0100

    UserDto toegevoegd. User by email endpoint toegevoegd. Searchbar toegevoegd.

--- RealDolmenInetum/RealDolmenAPI/Controllers/UserController.cs
+++ RealDolmenInetum/RealDolmenAPI/Controllers/UserController.cs
@@ -17,6 +17,9 @@ namespace RealDolmenAPI.Controllers
 
             // ENDPOINT ALS WE DE DATA VAN EEN BENCHER ZOUDEN WILLEN ZIEN 
             userGroup.MapGet("/{id:int}", async (int id, AppDbContext db) => await db.User.FirstOrDefaultAsync(u=>u.Id==id) is User user ? Results.Ok(user) : Results.NotFound());
+
+            // endpoint voor het zoeken van users op basis van email
+            userGroup.MapGet("/search", async (AppDbContext db, string email) => await db.User.Where(u => EF.Functions.Like(u.Email, $"%{email}%")).ToListAsync());
         }
     }
 }
