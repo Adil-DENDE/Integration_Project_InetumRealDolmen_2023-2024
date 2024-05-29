1defa3d9338b00654ae9a8a2ce6e67f581dc3724
Adil-DENDE <adil.lakhloufi@student.odisee.be>
2024-05-29 17:55:35 +0200

    Update van login/ pagina's secure maken ivm rollen
    Update van de UI van de Login pagina.
    Pagina toevoegen acces update.
    Pagina userdetails acces update.

---
+++ RealDolmenInetum/RealDolmenInetum/Components/Pages/Login/Login.razor.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using ModelLibrary.Dto;
+
+namespace RealDolmenInetum.Components.Pages.Login
+{
+    public partial class Login
+    {
+        private UserLoginDto userLoginModel = new UserLoginDto();
+        private string errorMessage = "";
+        private bool shouldShowInvalidLoginAlert = false;
+
+        private async Task HandleLogin(EditContext editContext)
+        {
+            var response = await Http.PostAsJsonAsync("https://localhost:7256/user/login", userLoginModel);
+            if (response.IsSuccessStatusCode)
+            {
+                var token = await response.Content.ReadAsStringAsync();
+                token.Trim().Trim('"');
+
+                auth.SetToken(token);
+                await LocalStorage.SetItemAsync("authToken", token);
+
+                NavigationManager.NavigateTo("/home");
+            }
+            else
+            {
+                errorMessage = "Inloggen mislukt. Controleer uw inloggegevens en probeer het opnieuw.";
+                userLoginModel.Email = "adil@gmail.com";
+                userLoginModel.Password = "Test123!";
+            }
+        }
+    }
+}
